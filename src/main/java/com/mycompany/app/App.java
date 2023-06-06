b2ba491cd5a703a842eaf3474bf42aedadd3456b
baoanth <baoanth@gmail.com>
2023-06-06 15:42:17 +0700

    Remove with lov3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World with LOV3!";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
